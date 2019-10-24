1553061551b35137d7bce4ffaef4f10b064915e7
Paulo Gabriel <paulo@janusproj.com>
2019-10-24 10:47:05 -0300

    add config for circleci

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (<div>Olá</div>);
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      list: [
+        {nome: 'Example', id: 1}
+      ]
+    }
+  }
+
+
+  render (){
+    const { list } = this.state;
+    return (
+        <div>
+          <ul>
+            {list.map(item => (
+                <li key={item}>{item.nome}</li>
+            ))}
+          </ul>
+        </div>
+    );
+  }
 }
 
 export default App;
